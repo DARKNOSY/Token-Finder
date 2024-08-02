210a17e2e5ce1cd4e1254ac82392b38faaf15d51
D4RKN0$6 <109553205+DARKNOSY@users.noreply.github.com>
2024-08-02 14:24:00 +0200

    Update launcher.bat

--- Token-Finder/launcher.bat
+++ Token-Finder/launcher.bat
@@ -1,4 +1,21 @@
 @echo off & cls
+color 3
+title Token Finder
+
+python --version >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Python is not installed. Installing now...
+    bitsadmin /transfer "DownloadPython" /priority high https://www.python.org/ftp/python/3.12.4/python-3.12.4-amd64.exe %temp%\python-3.12.4-amd64.exe
+    %temp%\python-3.12.4-amd64.exe /quiet InstallAllUsers=1 PrependPath=1
+    del %temp%\python-3.12.4-amd64.exe
+    python --version >nul 2>&1
+    if %errorlevel% neq 0 (
+        echo Python installation failed. Exiting...
+        pause
+        exit /b 1
+    )
+)
+cls
 
 py -m main
 
